cc564758c77e57e6aa72b0c2b368373f5ca59c28
Karla Saaremäe <karla.saaremae@tktk.ee>
2024-10-30 16:45:41 +0200

    homework

---
+++ public/scripts/app.js
@@ -0,0 +1,22 @@
+let getMoviePoster = document.querySelector(".movie-poster");
+let getMovieTitle = document.querySelector(".movie-title");
+let modal = document.querySelector("#myModal");
+let closeBtn = document.querySelector(".closeBtn");
+let modalImage = document.querySelector(".movie-poster-modal");
+let modalTitle = document.querySelector(".movie-title-modal");
+
+getMoviePoster.addEventListener("click", () => {
+  modal.style.display = "block";
+  modalImage.src = getMoviePoster.src;
+  modalTitle.textContent = getMovieTitle.textContent;
+});
+
+closeBtn.addEventListener("click", () => {
+  modal.style.display = "none";
+});
+
+window.addEventListener("click", (e) => {
+  if (e.target == modal) {
+    modal.style.display = "none";
+  }
+});
--- server.js
+++ server.js
@@ -46,6 +46,38 @@ app.post("/search", (req, res) => {
   );
 });
 
+app.get("/", async (req, res) => {
+  const movieId = 889737;
+
+  let url = `https://api.themoviedb.org/3/movie/${movieId}?api_key=b5ed8cede022fe9ef61ae22cc6a4dda1`;
+
+  axios.get(url).then((response) => {
+
+    let data = response.data;
+
+    let releaseDate = new Date(data.release_date).getFullYear();
+    let currentYear = new Date().getFullYear();
+
+    let genres = "";
+    data.genres.forEach((genre) => {
+      genres += genre.name + ", ";
+    });
+
+    let genresUpdated = genres.slice(0, -2) + ".";
+
+    let posterUrl = `https://image.tmdb.org/t/p/w600_and_h900_bestv2${data.poster_path}`;
+
+    res.render("index", {
+      dataToRender: response.data,
+      year: currentYear,
+      releaseDate: releaseDate,
+      genres: genresUpdated,
+      posterUrl: posterUrl,
+    });
+  });
+});
+
+
 app.listen(3000, () => {
   console.log("Server is running.");
 });
